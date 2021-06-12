bcee1e34096d1707501a9877ca80d685e558c16f
TanKiem <anhtanhangxom@gmail.com>
2021-06-12 14:59:31 +0700

    change bottom nav bar

--- lib/features/presentation/setting.dart
+++ lib/features/presentation/setting.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_clean_architecture/global/app_routes.dart';
-import 'package:flutter_app_clean_architecture/features/presentation/profile/widget/user_infomation_screen.dart';
-import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);
@@ -43,14 +41,7 @@ class _SettingScreenState extends State<SettingScreen> {
                   SizedBox(height: 60,),
                   InkWell(
                       onTap: (){
-                        pushNewScreenWithRouteSettings(
-                          context,
-                          settings: RouteSettings(name: AppRoutes.routeUserInfor),
-                          withNavBar: false,
-                          screen: UserInformation(),
-                          pageTransitionAnimation: PageTransitionAnimation.fade,
-                        );
-                        // Navigator.of(context).pushNamed(Constants.routeUserInfor);
+                        Navigator.of(context).pushNamed(AppRoutes.routeUserInfor);
                       },
                       child: ProfileMenu(
                           size: size, text: 'Tài khoản', 
@@ -58,7 +49,9 @@ class _SettingScreenState extends State<SettingScreen> {
                   ),
                   // Profile_Menu(size: size, text: 'Thông báo', icon: Icon(Icons.notifications_outlined,size: 30,color:  Colors.blue,)),
                   ProfileMenu(size: size, text: 'Trợ giúp', icon: Icon(Icons.contact_support_outlined,size: 30,color: Colors.blue,)),
-                  ProfileMenu(size: size, text: 'Đăng xuất', icon: Icon(Icons.exit_to_app_outlined,size: 30,color:  Colors.blue,)),
+                  ProfileMenu(size: size, text: 'Đăng xuất', icon: Icon(Icons.exit_to_app_outlined,size: 30,color:  Colors.blue,),
+                    onTap: _logout,
+                  ),
                 ],
 
           ),
@@ -66,43 +59,87 @@ class _SettingScreenState extends State<SettingScreen> {
       ),
     );
   }
+
+  void _logout() {
+    showDialog(context: context, builder: (_) => AlertDialog(
+      title: Text("Đăng xuất"),
+      content: Text("Bạn có muốn đăng xuất?"),
+      actions: [
+        GestureDetector(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              "Đồng ý",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                color: Colors.blue
+              ),
+            ),
+          ),
+          onTap: (){
+            Navigator.of(context).pushNamedAndRemoveUntil(AppRoutes.routeLogin, (route) => false);
+          },
+        ),
+        GestureDetector(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              "Hủy",
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.red
+              ),
+            ),
+          ),
+          onTap: (){
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    ));
+  }
 }
 
 class ProfileMenu extends StatelessWidget {
   const ProfileMenu({
     Key? key,
     required this.size,required this.text, required this.icon,
+    this.onTap
   }) : super(key: key);
 
   final Size size;
   final String text;
   final Icon icon;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top:10),
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 15),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
-              child: icon,
-            ),
-            Text(text,
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 15,
-                  fontWeight: FontWeight.w600
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.only(top:10),
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                child: icon,
               ),
-            ),
-          ],
+              Text(text,
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 15,
+                    fontWeight: FontWeight.w600
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
