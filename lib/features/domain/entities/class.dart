a947877d43467887f824ab9f728d8eb508a6d9fd
Tân Kiêm <anhtanhangxom@gmail.com>
2021-06-14 17:40:25 +0700

    list schedule

---
+++ lib/features/domain/entities/class.dart
@@ -0,0 +1,43 @@
+import 'schedule.dart';
+
+class Class {
+  Class({
+    required this.id,
+    required this.maKyHoc,
+    required this.maLopHoc,
+    required this.tenLopHoc,
+    required this.maMonHoc,
+    required this.tenMonHoc,
+    required this.lichHoc,
+  });
+
+  final String id;
+  final String maKyHoc;
+  final String maLopHoc;
+  final String tenLopHoc;
+  final String maMonHoc;
+  final String tenMonHoc;
+  final List<LichHoc> lichHoc;
+
+  factory Class.fromJson(Map<String, dynamic> json) => Class(
+    id: json["_id"],
+    maKyHoc: json["maKyHoc"],
+    maLopHoc: json["maLopHoc"],
+    tenLopHoc: json["tenLopHoc"],
+    maMonHoc: json["maMonHoc"],
+    tenMonHoc: json["tenMonHoc"],
+    lichHoc: List<LichHoc>.from(json["lichHoc"].map((x) => LichHoc.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "_id": id,
+    "maKyHoc": maKyHoc,
+    "maLopHoc": maLopHoc,
+    "tenLopHoc": tenLopHoc,
+    "maMonHoc": maMonHoc,
+    "tenMonHoc": tenMonHoc,
+    "lichHoc": List<dynamic>.from(lichHoc.map((x) => x.toJson())),
+  };
+}
+
+
---
+++ lib/features/domain/entities/schedule.dart
@@ -0,0 +1,39 @@
+class LichHoc {
+  LichHoc({
+    required this.tuanHoc,
+    required this.phongHoc,
+    required this.thuHoc,
+    required this.tietBatDau,
+    required this.tietKetThuc,
+    required this.thoiGianBatDau,
+    required this.thoiGianKetThuc,
+  });
+
+  final List<int> tuanHoc;
+  final String phongHoc;
+  final int thuHoc;
+  final int tietBatDau;
+  final int tietKetThuc;
+  final String thoiGianBatDau;
+  final String thoiGianKetThuc;
+
+  factory LichHoc.fromJson(Map<String, dynamic> json) => LichHoc(
+    tuanHoc: List<int>.from(json["tuanHoc"].map((x) => x)),
+    phongHoc: json["phongHoc"],
+    thuHoc: json["thuHoc"],
+    tietBatDau: json["tietBatDau"],
+    tietKetThuc: json["tietKetThuc"],
+    thoiGianBatDau: json["thoiGianBatDau"],
+    thoiGianKetThuc: json["thoiGianKetThuc"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "tuanHoc": List<dynamic>.from(tuanHoc.map((x) => x)),
+    "phongHoc": phongHoc,
+    "thuHoc": thuHoc,
+    "tietBatDau": tietBatDau,
+    "tietKetThuc": tietKetThuc,
+    "thoiGianBatDau": thoiGianBatDau,
+    "thoiGianKetThuc": thoiGianKetThuc,
+  };
+}
